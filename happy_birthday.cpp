188705129c31ab694cb98469f97bc5d536bb5927
bipasa-sardar <bipasasardar96123@gmail.com>
2025-04-20 21:50:56 +0530

    Add files via upload

---
+++ happy_birthday.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+void happybirthday(std::string name);
+int main(){
+    std:: string name="bipasa";
+    happybirthday(name);
+    return 0;
+}
+void happybirthday(std::string name){
+    std::cout<<"happy birthday to you\n";
+    std::cout<<"happy birthday to you\n";
+    std::cout<<"happy birthday to you, dear "<<name<<"\n";
+    std::cout<<"happy birthday to you\n";
+}
